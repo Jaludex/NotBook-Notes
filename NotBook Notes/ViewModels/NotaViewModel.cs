1b62a24cfae2113ddc83c9eb9f9bb74d8ad73b67
Rosa Rosales <rosamilib@gmail.com>
2024-11-26 03:14:52 -0400

    Creando el view model para la futura vista de las notas usando Data Binding

---
+++ NotBook Notes/ViewModels/NotaViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace NotBook_Notes.ViewModels
+{
+    class NotaViewModel : INotifyPropertyChanged
+    {
+        public string _Titulo;
+        public string _Contenido;
+        public DateTime _FechaCreacion;
+
+        public string Contenido
+        {
+            get => _Contenido;
+            set
+            {
+                if (_Contenido != value)
+                {
+                    _Contenido = value;
+                    OnPropertyChanged(nameof(Contenido));  // Notifica a la vista sobre el cambio
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
