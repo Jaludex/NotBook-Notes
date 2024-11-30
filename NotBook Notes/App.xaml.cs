98725a1af49ec7a14e1f833749b106b1de090c54
Jaludex <53153684+Jaludex@users.noreply.github.com>
2024-11-30 05:33:14 -0400

    Pedir desactivar las optimizaciones y el ahorro de energia al primer inicio

--- NotBook Notes/App.xaml.cs
+++ NotBook Notes/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using NotBook_Notes.Models;
+﻿using Android.Content;
+using Android.OS;
+using Android.Provider;
+using NotBook_Notes.Models;
 using NotBook_Notes.Views;
 using Plugin.LocalNotification;
 using Plugin.LocalNotification.EventArgs;
@@ -40,6 +43,29 @@ namespace NotBook_Notes
                 ManejoDeDatos.nombreUsuario = string.IsNullOrEmpty(username) ? "Usuario" : username;
 
                 await Application.Current.MainPage.DisplayAlert("¡Todo Listo!", $"A partir de ahora, te llamaré {ManejoDeDatos.nombreUsuario}, te queda muy bien.", "¡Vamos!");
+
+                bool respuesta = await Application.Current.MainPage.DisplayAlert("Una Ultima Cosa...", $"{ManejoDeDatos.nombreUsuario}, en los dispositivos de hoy en dia, las optimizaciones de tu dispositivo puede acabar con nuestro preciado servicio de notificaciones.{System.Environment.NewLine}Para que no sufras ese problema, puedes ir a la opciones de la app y buscar un menu del estilo 'Ahorro de Bateria' y desactivarlo, o seleccionar 'Sin reestricciones'.{System.Environment.NewLine}{System.Environment.NewLine}¿Quieres ir a las configuraciones de la app ahora?", "Configuraciones", "No Importa");
+                if (respuesta)
+                {
+                    var intent = new Intent();
+                    intent.SetAction(Settings.ActionRequestIgnoreBatteryOptimizations);
+                    intent.SetData(Android.Net.Uri.FromParts("package", Android.App.Application.Context.PackageName, null));
+                    intent.AddFlags(ActivityFlags.NewTask);
+
+                    // Verifica si se encontro la ventana de optimizar memoria
+                    if (intent.ResolveActivity(Android.App.Application.Context.PackageManager) != null)
+                    {
+                        Android.App.Application.Context.StartActivity(intent);
+                    }
+                    //Si no, abre las opciones comunes de la app
+                    else
+                    {
+                        intent = new Intent(Android.Provider.Settings.ActionApplicationDetailsSettings);
+                        intent.AddFlags(ActivityFlags.NewTask);
+                        intent.SetData(Android.Net.Uri.FromParts("package", Android.App.Application.Context.PackageName, null));
+                        Android.App.Application.Context.StartActivity(intent);
+                    }
+                }
 
                 string ruta = Path.Combine(ManejoDeDatos.GetRutaBackups(), "backup.json");
                 await ManejoDeDatos.GuardarDatosJSONAsync(ruta);
